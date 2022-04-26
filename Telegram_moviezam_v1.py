2d21b0116bf28ab6c4e77f67393caf772b97eab8
LuisCioffi <luigicio@hotmail.com>
2022-04-26 21:53:53 +0200

    adjusted

--- Telegram_moviezam_v1.py
+++ Telegram_moviezam_v1.py
@@ -33,7 +33,6 @@ from telegram.ext import (
 
 
 
-
 # upload w2v model
 model_w2v = pd.read_pickle('pickle_w2v')
 X_word2vec = pd.read_pickle('pickle_w2v_vectors')
@@ -46,19 +45,19 @@ data_movielens_final_new['Links'] = 'https://www.imdb.com/title/tt'+data_moviele
 
 # functions for search and reccomandation
 def search_plot_similarity_adjusted(text,encoding_method,encoded_corpus,dataframe,topk,model=None,vectorizer=None,features=None):
-	TOKEN_LEMMA = [clean_text_lemma(text)]
-	space_query=encoding_method(TOKEN_LEMMA,model)
-	cosineSimilarities = cosine_similarity(space_query, encoded_corpus).flatten()
-	df=dataframe.iloc[np.argsort(cosineSimilarities)[-topk*20:].tolist() ]
-	df['similarity']=np.sort(cosineSimilarities)[-topk*20:].tolist() 
-	df['final_score']=(df['similarity']*0.87)+(df['votes_score']*0.13)
-	df=df.sort_values(['final_score'], ascending=False)
-	df=df.groupby('Title').first().reset_index()
-	df=df.nlargest(topk,'final_score')
+    TOKEN_LEMMA = [clean_text_lemma(text)]
+    space_query=encoding_method(TOKEN_LEMMA,model)
+    cosineSimilarities = cosine_similarity(space_query, encoded_corpus).flatten()
+    df=dataframe.iloc[np.argsort(cosineSimilarities)[-topk*20:].tolist() ]
+    df['similarity']=np.sort(cosineSimilarities)[-topk*20:].tolist() 
+    df['final_score']=(df['similarity']*0.87)+(df['votes_score']*0.13)
+    df=df.sort_values(['final_score'], ascending=False)
+    df=df.groupby('Title').first().reset_index()
+    df=df.nlargest(topk,'final_score')
     
-	df=df[['Title','Links']]
+    df=df[['Title','Links']]
 
-	return df
+    return df
 def word2vec_encoding_query(text,model_w2v,percentage_of_vocabulary_to_use=None, min_count=5):
     size=300
     corpus = []
@@ -106,45 +105,49 @@ def clean_text_lemma(text):
     return temp
 
 
-
 def search_movie_similarity_adjusted(title,encoding_method,encoded_corpus,dataframe,topk,model=None,vectorizer=None,features=None):
-	df=dataframe
-	 # ratio method is more accurate , but slower
-	df['sim'] = [difflib.SequenceMatcher(a=title.lower(), b=i.lower()).quick_ratio() for i in df['Title']   ]   
-	df=df.sort_values(['sim'], ascending=False)
-	df1=df.head(1)
-	title_found=df1['Title'].item()
-	df2=df[df['Title']==title_found]
-	df2=df2.dropna(subset=['text_clean'])
-	df2['doc_len'] = df2['text_clean'].apply(lambda words: len(words.split()))
-	df2=df2.sort_values(['doc_len'], ascending=False)
-	df2=df2.head(1)
-	df2=df2.drop(['sim'], axis=1)
-	query_new=df2['text_clean'].item()
-	query_new
+    df=dataframe
+     # ratio method is more accurate , but slower
+    df['sim'] = [difflib.SequenceMatcher(a=title.lower(), b=i.lower()).ratio() for i in df['Title']   ]   
+    df=df.sort_values(['sim'], ascending=False)
+    df1=df.head(1)
+    title_found=df1['Title'].item()
+    df2=df[df['Title']==title_found]
+    df2=df2.dropna(subset=['text_clean'])
+    df2['doc_len'] = df2['text_clean'].apply(lambda words: len(words.split()))
+    df2=df2.sort_values(['doc_len'], ascending=False)
+    df2=df2.head(1)
+    df2=df2.drop(['sim'], axis=1)
+    query_new=df2['text_clean'].item()
+    query_new
 
 #
-	# print("Most similar to:" + title_found)
-	# print("Top " +str(topk)+ " titles, by plot similarity")
-	df_new=search_plot_similarity_adjusted(text=query_new,encoding_method=encoding_method,encoded_corpus=encoded_corpus,dataframe=dataframe,topk=topk,model=model,vectorizer=vectorizer,features=features)
-	df_new=df_new[df_new['Title']!=title_found] 
-	df_new['plot_searched']=query_new
-	df_new['Title_to_search']=title_found
-	cols = df_new.columns.tolist()
-	cols = cols[-1:] + cols[:-1]
-	df_new = df_new[cols]
-	return df_new
+    # print("Most similar to:" + title_found)
+    # print("Top " +str(topk)+ " titles, by plot similarity")
+    df_new=search_plot_similarity_adjusted(text=query_new,encoding_method=encoding_method,encoded_corpus=encoded_corpus,dataframe=dataframe,topk=topk,model=model,vectorizer=vectorizer,features=features)
+    df_new=df_new[df_new['Title']!=title_found] 
+    df_new['plot_searched']=query_new
+    df_new['Title_to_search']=title_found
+    cols = df_new.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    df_new = df_new[cols]
+    return df_new
+
 
 
 
 
 # here start the telegram chatbot
+from logging import basicConfig, getLogger, INFO
+
+basicConfig(level=INFO)
+log = getLogger()
 
 
 # Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+#logging.basicConfig(
+ #   format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+#)
 TOKEN='5389959641:AAFzapxb0HqNfNtEKCBrpVJzTx5AexVX5Bk'
 
 # State definitions for top level conversation
@@ -163,8 +166,8 @@ def start(update: Update, context: CallbackContext) -> int:
 
     keyboard = [
         [
-            InlineKeyboardButton("Search movie by plot events", callback_data=str(ONE)),
-            InlineKeyboardButton("Suggest movies by similarity", callback_data=str(TWO)),
+            InlineKeyboardButton("Search by plot events", callback_data=str(ONE)),
+            InlineKeyboardButton("Suggest movies", callback_data=str(TWO)),
         ]
     ]
 
@@ -176,9 +179,17 @@ def start(update: Update, context: CallbackContext) -> int:
         "Hi  ! I'm" "* Moviezam!* \U0001F916" " I know everything about""*  Movies!* \U0001F4FD" "\n" 
         "Do you want to search for movies with" "* a particular plot event?*" " \n" 
         "Can i ""*suggest you similar movies *""to the ones you like?\n" 
-        "Beware: my catalog is updated " "*until 2019*" "\n" 
-        "*Choose one*"" of the options below, I will do a search for you \U0001F603 !" "\n"
+         "Beware: my catalog is updated " "*until 2019*"  "\U0001F622" "\n" 
          "To abort, simply type /stop.",
+        #   reply_markup=reply_markup,parse_mode= 'Markdown',
+    parse_mode= 'Markdown',
+    )
+    
+    
+    
+    update.message.reply_text(
+
+        "*Choose one*"" of the options below, I will do a search for you \U0001F603 !" "\n",
         #   reply_markup=reply_markup,parse_mode= 'Markdown',
         reply_markup=reply_markup ,parse_mode= 'Markdown',
     )
@@ -191,7 +202,8 @@ def answer_recommend(update: Update, context: CallbackContext) -> int:
     query = update.callback_query
     query.answer()
     query.edit_message_text(
-        text="'Please write the title of a movie that you like, i will suggest you 5 similar movies'"
+        text="Please write the *title* of a movie that you like, i will suggest you 5 similar movies \n"
+        "Beware that the movie you submit is the same i found in my database",parse_mode= 'Markdown'
     )
 
     return QUERY_TITLE
@@ -211,7 +223,7 @@ def answer_plot(update: Update, context: CallbackContext) -> int:
     query = update.callback_query
     query.answer()
     query.edit_message_text(
-        text="Please write some plot events or details and i will show you some related movies"
+        text="Please write some *plot events or details* and i will show you some related movies",parse_mode= 'Markdown'
     )
     return QUERY_PLOT
 
@@ -256,12 +268,14 @@ def echo_plot(update: Update, context: CallbackContext) -> None:
 
 def echo_title(update: Update, context: CallbackContext) -> None:
     text_to_search=update.message.text
-    df=search_movie_similarity_adjusted(title=title_search,encoding_method=word2vec_encoding_query,model=model_w2v,encoded_corpus=X_word2vec,dataframe=data_movielens_final_new,topk=10)
+    update.message.reply_text('Wait please... \U0001F605 ') 
+    df=search_movie_similarity_adjusted(title=text_to_search,encoding_method=word2vec_encoding_query,model=model_w2v,encoded_corpus=X_word2vec,dataframe=data_movielens_final_new,topk=10)
     df_lista=df['Links'].tolist()
     df_lista_titles=df['Title'].tolist()
     title_found=df['Title_to_search'].tolist()
     title_found=title_found[0]
-    update.message.reply_text(f'Title found in database : {title_found}')  
+    update.message.reply_text(f'You submit the movie title : {text_to_search}') 
+    update.message.reply_text(f'I found in my database the following movie title : {title_found}')  
     update.message.reply_text(f'Below you can find 5 most similar movies to:  {title_found}')  
     update.message.reply_text(f'Movie 1: {df_lista_titles[0]}')                         
     update.message.reply_text(df_lista[0])
@@ -301,8 +315,8 @@ def startover(update: Update, context: CallbackContext) -> None:
 
     keyboard = [
         [
-            InlineKeyboardButton("Search movie by plot events", callback_data=str(ONE)),
-            InlineKeyboardButton("Suggest movies by similarity", callback_data=str(TWO)),
+            InlineKeyboardButton("Search by plot events", callback_data=str(ONE)),
+            InlineKeyboardButton("Suggest movies", callback_data=str(TWO)),
         ]
     ]
 
@@ -342,7 +356,7 @@ def stop2(update: Update, context: CallbackContext) -> int:
 def main() -> None:
     """Run the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater(TOKEN)
+    updater = Updater(token=TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
@@ -379,7 +393,7 @@ def main() -> None:
     dispatcher.add_handler(conv_handler)
 
     # Start the Bot
-    updater.start_polling()
+    updater.start_polling(timeout=100)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
